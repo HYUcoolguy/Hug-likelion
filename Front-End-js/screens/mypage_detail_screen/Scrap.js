30b9fee7a8dec2d3e8aa23076570248d7acb4dbb
Sanghyo <sanghyoboy@naver.com>
2020-08-04 16:42:46 +0900

    add scrap grid page

--- Front-End-js/screens/mypage_detail_screen/Scrap.js
+++ Front-End-js/screens/mypage_detail_screen/Scrap.js
@@ -4,24 +4,32 @@ import {
   View,
   Text,
   SafeAreaView,
+  TouchableOpacity,
   Dimensions,
   TextInput
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Grid from "../../component/Grid";
 
 export default function Scrap() {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
+      <Grid />
     </SafeAreaView>
   );
 }
 
-const Header = () => {
+const Header = ({ navigation }) => {
   return (
     <View style={styles.headerContainer}>
+      <View style={styles.header_left}></View>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>스크랩 게시물</Text>
       </View>
+      <TouchableOpacity style={styles.icon_container}>
+        <Ionicons name="ios-add" size={23} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -33,17 +41,28 @@ const styles = StyleSheet.create({
     paddingTop: 90
   },
   headerContainer: {
+    flexDirection: "row",
     alignItems: "center",
     borderBottomWidth: 1,
     borderBottomColor: "gray",
     justifyContent: "space-between",
     paddingBottom: 10
   },
+  header_left: {
+    flex: 1,
+    marginLeft: 15
+  },
   titleContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center"
   },
   title: {
-    fontSize: 25
+    fontSize: 23
+  },
+  icon_container: {
+    flex: 1,
+    alignItems: "flex-end",
+    marginRight: 15
   }
 });
